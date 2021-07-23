62104c736934354cd84418a447e51d8e2554cc10
sone <alftaketomo@gmail.com>
2021-07-23 02:40:15 +0000

    set invert alp output

--- alp.sh
+++ alp.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo cat /var/log/nginx/access.log | alp ltsv -m "/new_items/.*.json,/items/.*.json,/users.*.json,/transactions/.*.png,/upload/.*.jpg,/static/(js|css)"
+sudo cat /var/log/nginx/access.log | alp ltsv -r -m "/new_items/.*.json,/items/.*.json,/users.*.json,/transactions/.*.png,/upload/.*.jpg,/static/(js|css)"
